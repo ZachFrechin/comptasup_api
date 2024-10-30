5a4af29638879e3e6bd366cf51d0d23efd920550
Jok3Noname <63104855+JackDonna@users.noreply.github.com>
2024-10-30 15:33:54 +0100

    dockerizing & nginx server

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return response()->json([
+        'message' => 'Welcome to the API!',
+        ]);
+});
